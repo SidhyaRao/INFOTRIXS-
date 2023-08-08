1e25c6358d2f7675c09c85fcf32d86b1a5f07717
SidhyaRao <141553969+SidhyaRao@users.noreply.github.com>
2023-08-08 18:56:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,49 +1,28 @@
 
-import json
+def add_contacts(name, phone, email, contacts_list):
+    new_contact = {'name': name, 'phone': phone, 'email': email}
+    contacts_list.append(new_contact)
 
-def load_contacts():
-    try:
-        with open('contacts.json', 'r') as f:
-            return json.load(f)
-    except FileNotFoundError:
-        return []
-
-def save_contacts(contacts):
-    with open('contacts.json', 'w') as f:
-        json.dump(contacts, f)
-
-def add_contact(name, phone, email):
-    contacts = load_contacts()
-    contacts.append({'name': name, 'phone': phone, 'email': email})
-    save_contacts(contacts)
-
-def search_contact(name):
-    contacts = load_contacts()
-    found_contacts = [contact for contact in contacts if contact['name'].lower() == name.lower()]
+def search_contact(name, contacts_list):
+    found_contacts = []
+    for contact in contacts_list:
+        if contact['name'] == name:
+            found_contacts.append(contact)
     return found_contacts
 
-def delete_contact(name):
-    contacts = load_contacts()
-    contacts = [contact for contact in contacts if contact['name'].lower() != name.lower()]
-    save_contacts(contacts)
+def delete_contact(name, contacts_list):
+    for contact in contacts_list:
+        if contact['name'] == name:
+            contacts_list.remove(contact)
 
-def update_contact(name, phone, email):
-    contacts = load_contacts()
-    for contact in contacts:
-        if contact['name'].lower() == name.lower():
+def update_contact(name, phone, email, contacts_list):
+    for contact in contacts_list:
+        if contact['name'] == name:
             contact['phone'] = phone
             contact['email'] = email
-            break
-    save_contacts(contacts)
-
-def print_contacts(contacts):
-    if not contacts:
-        print("No contacts found.")
-    else:
-        for contact in contacts:
-            print(f"Name: {contact['name']}, Phone: {contact['phone']}, Email: {contact['email']}")
 
 def main():
+    contacts = []  # Initialize an empty list to store contacts
     while True:
         print("\nContact Management System")
         print("1. Add Contact")
@@ -58,24 +37,24 @@ def main():
             name = input("Enter the name: ")
             phone = input("Enter the phone number: ")
             email = input("Enter the email address: ")
-            add_contact(name, phone, email)
+            add_contacts(name, phone, email, contacts)
             print("Contact added successfully.")
 
         elif choice == '2':
             name = input("Enter the name to search: ")
-            found_contacts = search_contact(name)
+            found_contacts = search_contact(name, contacts)
             print_contacts(found_contacts)
 
         elif choice == '3':
             name = input("Enter the name to delete: ")
-            delete_contact(name)
+            delete_contact(name, contacts)
             print("Contact deleted successfully.")
 
         elif choice == '4':
             name = input("Enter the name to update: ")
             phone = input("Enter the new phone number: ")
             email = input("Enter the new email address: ")
-            update_contact(name, phone, email)
+            update_contact(name, phone, email, contacts)
             print("Contact updated successfully.")
 
         elif choice == '5':
@@ -85,6 +64,12 @@ def main():
         else:
             print("Invalid choice. Please try again.")
 
+def print_contacts(contacts):
+    if not contacts:
+        print("No contacts found.")
+    else:
+        for contact in contacts:
+            print(f"Name: {contact['name']}, Phone: {contact['phone']}, Email: {contact['email']}")
+
 if __name__ == "__main__":
     main()
-
